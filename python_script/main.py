d653965d8f56a70478b25041d63f4236160743d4
Yanic von Moos <yanic@Yanics-MacBook-Pro.local>
2025-11-02 21:58:20 +0100

    feat: added the unfinished function solve_game

--- python_script/main.py
+++ python_script/main.py
@@ -3,8 +3,7 @@ from functions.move_plate import move_plate
 
 
 game = Pillars(3)
-print(game)
-
+autosolve_game = Pillars(3)
 move_plate(game, "p1", "p3")
 move_plate(game, "p1", "p2")
 move_plate(game, "p3", "p2")
@@ -12,5 +11,8 @@ move_plate(game, "p1", "p3")
 move_plate(game, "p2", "p1")
 move_plate(game, "p2", "p3")
 move_plate(game, "p1", "p3")
+
+def solve_game(autosolve_game):
+    yield
 
 print(game)
